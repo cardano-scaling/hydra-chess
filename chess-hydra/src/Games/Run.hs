e68b28c4524f7c9399101e3d07fed5a439a64005
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-01-13 16:11:18 +0100

    Minor refactoring of frontend creation

--- chess-hydra/src/Games/Run.hs
+++ chess-hydra/src/Games/Run.hs
@@ -8,7 +8,6 @@ module Games.Run where
 import Cardano.Binary (serialize')
 import Control.Monad (forever)
 import Control.Monad.Class.MonadTimer (threadDelay)
-import Game.Chess (Chess)
 import Game.Client (runClient)
 import Game.Client.Console (inputParser, mkImpureIO)
 import Games.Logging (findLogFile, withLogger)
@@ -18,25 +17,31 @@ import Games.Run.Hydra (HydraNode (..), withHydraNode)
 import Games.Server.Hydra (HydraParty (..), withHydraServer)
 import Games.Server.IO (notifyChessEvent)
 import Options.Applicative (execParser)
-import System.IO (BufferMode (..), hSetBuffering, stdout)
+import System.IO (BufferMode (..), hSetBuffering, stdout, stdin, hIsTerminalDevice)
 
 run :: IO ()
 run = do
   Options{cardanoNetwork, onlyCardano} <- execParser hydraGamesInfo
+  frontend <- hIsTerminalDevice stdin >>= \case
+    True -> pure mkTerminalFrontend
+    False -> error "Not implemented"
   hSetBuffering stdout NoBuffering
   logFile <- findLogFile cardanoNetwork
   withLogger logFile $ \logger ->
     withCardanoNode logger cardanoNetwork $ \cardano ->
       if onlyCardano
         then runCardanoClient
-        else startServers logger cardano
+        else startServers logger cardano frontend
  where
   runCardanoClient =
     forever (threadDelay 60_000_000)
 
-  startServers logger cardano@CardanoNode{network} =
+  mkTerminalFrontend server =
+    runClient server notifyChessEvent (mkImpureIO inputParser)
+
+  startServers logger cardano@CardanoNode{network} frontend =
     withHydraNode logger cardano $ \HydraNode{hydraParty, hydraHost} -> do
       let party = HydraParty $ serialize' hydraParty
       withHydraServer logger network party hydraHost $ \server -> do
         putStrLn $ "Starting client for " <> show party <> " and host " <> show hydraHost
-        runClient @Chess @_ @_ server notifyChessEvent (mkImpureIO inputParser)
+        frontend server
