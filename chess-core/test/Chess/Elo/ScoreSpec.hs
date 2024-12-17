2d00cd930c0f6063ddf9bd038a06a46f8f4e0321
Arnaud Bailly <arnaud.bailly@iohk.io>
2024-12-17 03:17:24 +0100

    Check gains are symetric up to a difference of 400
    Actually they aren't exactly symetric due to rounding errors when
    computing the probability table.

--- chess-core/test/Chess/Elo/ScoreSpec.hs
+++ chess-core/test/Chess/Elo/ScoreSpec.hs
@@ -1,11 +1,34 @@
+{-# OPTIONS_GHC -Wno-orphans #-}
+
 module Chess.Elo.ScoreSpec where
 
 import Chess.Elo.Score (Result (..), eloGain)
+import Data.Function ((&))
 import Test.Hspec (Spec, it, shouldBe)
+import Test.Hspec.QuickCheck (prop)
+import Test.QuickCheck (Arbitrary, counterexample, elements, forAll, suchThat)
+import Test.QuickCheck.Arbitrary (Arbitrary (..))
 
 spec :: Spec
-spec =
+spec = do
   it "can compute Elo gains as a change for each player" $ do
     eloGain 1800 2005 AWin `shouldBe` 15
     eloGain 1800 2005 BWin `shouldBe` -5
     eloGain 1800 2005 Draw `shouldBe` 5
+  prop "gains are (nearly) symetric for wins with less than 400 margin" $
+    forAll arbitrary $ \(Elo a) ->
+      forAll (arbitrary `suchThat` \(Elo b) -> abs (a - b) < 400) $ \(Elo b) ->
+        abs (eloGain a b AWin + eloGain b a BWin) <= 1
+          & counterexample ("a wins: " <> show (eloGain a b AWin) <> ", b wins: " <> show (eloGain b a BWin))
+
+newtype Elo = Elo Integer
+  deriving (Eq, Show)
+
+instance Arbitrary Elo where
+  arbitrary = Elo <$> elements [1000 .. 3000]
+  shrink (Elo x)
+    | x > 1000 = Elo <$> shrink x
+    | otherwise = []
+
+instance Arbitrary Result where
+  arbitrary = elements [AWin, BWin, Draw]
